3bdc848c1bc6e3a3833fe089919c3033fac416b7
mxrsel <marsel14082006@gmail.com>
2024-06-14 18:27:48 +0600

    finished app for joke from Chuck Norris

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react';
+import Joke from './components/Joke';
+import Button from './components/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface JokeResponse {
+  value: string;
 }
 
-export default App
+const App: React.FC<JokeResponse>= () => {
+  const [joke, setJoke] = useState<string>('');
+
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch('https://api.chucknorris.io/jokes/random');
+      const data: JokeResponse = await response.json();
+      setJoke(data.value);
+    } catch (error) {
+      console.log('Error fetching joke:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Chuck Norris jokes</h1>
+      <Joke joke={joke}/>
+      <Button onClick={fetchJoke}/>
+      
+    </div>
+  );
+};
+
+export default App;
